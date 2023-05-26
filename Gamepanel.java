ba0b65678c1160c4c12d199378dd1100c49ec54b
Jubaer Ahmed Bhuiyan <90813553+jabhuiyan@users.noreply.github.com>
2023-05-26 00:28:36 -0230

    Create Gamepanel.java

---
+++ Gamepanel.java
@@ -0,0 +1,16 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+
+public class Gamepanel extends JPanel implements ActionListener {
+  
+  GamePanel(){
+  
+  }
+  
+  @Override
+  public void actionPerformed(ActionEvent e){
+  
+  }
+
+}
